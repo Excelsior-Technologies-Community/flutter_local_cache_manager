08358faf704e02b5f171a5b3ffe36218e066cc56
Rutik1701 <parmarrutik882@gmail.com>
2026-02-05 12:11:31 +0530

    library

---
+++ local_cache_manager/lib/flutter_local_cache_manager.dart
@@ -0,0 +1,5 @@
+library flutter_local_cache_manager;
+
+export 'src/cache_item.dart';
+export 'src/cache_keys.dart';
+export 'src/local_cache_manager.dart';
---
+++ local_cache_manager/lib/src/cache_keys.dart
@@ -0,0 +1,5 @@
+class CacheKeys {
+  static const users = 'users_cache';
+  static const products = 'products_cache';
+  static const profile = 'profile_cache';
+}
